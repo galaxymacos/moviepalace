9b254ccafff12cf69a68e7a834804306001d5be5
xunruan <xunruan@icloud.com>
2022-08-18 17:57:35 -0400

    Initial commit

---
+++ moviefacts/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import Template, Context
+
+
+# Create your views here.
+def citizen_kane(request):
+    content = """{{movie}} was released in {{year}}."""
+    template = Template(content)
+    context = Context({"movie": "Citizen Kane", "year": 1941})
+
+    result = template.render(context)
+    return HttpResponse(result)
